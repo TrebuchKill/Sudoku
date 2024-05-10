89ab23b6f55a7dbdd647ee5f983aa3be7e619080
TrebuchKill <bartwinski-hubert@web.de>
2024-05-10 15:09:05 +0200

    Updated Kotlin to 2.0.0-RC3, kotlinx-coroutines to 1.8.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,10 @@
 plugins {
 
-    kotlin("jvm") version "2.0.0-RC2"
+    kotlin("jvm") version "2.0.0-RC3"
 
     // Plugin to interact with the compose compiler, which is built-in into Kotlin since Kotlin Version 2.0.0-RC2
     // same version as Kotlin version
-    kotlin("plugin.compose") version "2.0.0-RC2"
+    kotlin("plugin.compose") version "2.0.0-RC3"
 
     // Actual Compose Library Version
     id("org.jetbrains.compose") version "1.6.10-rc01"
@@ -32,9 +32,9 @@ dependencies {
     }
 
     // The kotlin-stdlib-jdk[78] for version 1.8.20 are being added from somewhere, without the following line; but it seems to be optional
-    implementation(platform("org.jetbrains.kotlin:kotlin-bom:2.0.0-RC2"))
+    implementation(platform("org.jetbrains.kotlin:kotlin-bom:2.0.0-RC3"))
 
     // Use the (at the time) latest version of kotlinx-coroutines
-    implementation(platform("org.jetbrains.kotlinx:kotlinx-coroutines-bom:1.8.0"))
+    implementation(platform("org.jetbrains.kotlinx:kotlinx-coroutines-bom:1.8.1"))
     implementation(compose.desktop.currentOs)
 }
